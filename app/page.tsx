1f8fad8d87d0567aa054c9ec795cd084ef05a33a
guga-ggw <ggwomega15@gmail.com>
2024-01-28 15:48:42 +0400

    st

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+import BarImage from './assets/Bar.png';
+import LogoImage from './assets/arena_logo.png'
+import './index.scss'
+
+
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-full h-fit bg-red-500">
+      <nav className="w-full h-16 bg-white flex justify-around items-center ">
+        <img src={BarImage.src} alt="" />
+        <img src={LogoImage.src} alt="" />
+        <div className='w-12 h-6 bg-black rounded-xl'></div>
+      </nav>
     </main>
   );
 }

18325f028b18b3973bbea18fbaba1db797e49d06
guga-ggw <ggwomega15@gmail.com>
2024-02-01 14:36:02 +0400

    mn

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,17 @@ import nav_bg from './assets/nav_bgs.png'
 import nav_bottom_war from './assets/nav_bottom_war.png'
 import nav_left_war from './assets/nav_left_war.png'
 import nav_right_war from './assets/nav_right_war.png'
+import abt_btn from './assets/about+btn.png'
+import res_arena_inf from './assets/inf_res.png'
+import res_arena_char from './assets/res_chart.png'
+import strokes from './assets/strokes.png'
+import star from './assets/star.png'
+import token_1 from './assets/token1.png'
+import token_2 from './assets/token2.png'
+import token_3 from './assets/token3.png'
+import token_4 from './assets/token4.png'
+import token_5 from './assets/token5.png'
+import token_6 from './assets/token6.png'
 
 import './index.scss';
 import useWindowResize from './hooks/useWindowResize';
@@ -27,7 +38,32 @@ import { useState } from 'react';
 export default function Home() {
 
   const navlist = ['Home', 'About Us', 'Design concept', 'Roadmap', 'Tokenomics', 'Our team', 'Partners', 'Whitepaper', 'Arena Master Deck']
-
+  const token_list = [
+    {
+      text : "Competing in PVE battles to win leaderboard prizes",
+      img : token_1
+    },
+    {
+      text : "Competing in PVP battles against players",
+      img : token_2
+    },
+    {
+      text : "NFT Rewards ",
+      img : token_3
+    },
+    {
+      text : "Single Asset staking",
+      img : token_4
+    },
+    {
+      text : "In-game rewards",
+      img : token_5
+    },
+    {
+      text : "In-game bonuses",
+      img : token_6
+    },
+  ]
   const [navtoggle, settoggle] = useState(false)
 
 
@@ -63,8 +99,9 @@ export default function Home() {
           <div className='w-12 h-[24px] bg-black rounded-xl'></div>
       </nav>
       <div className='min-h-[700px] pb-2 bg-[#04131B] w-full px-4 flex font-black pt-14 flex-col relative md:text-center md:items-center'>
+        <img className='absolute right-8 top-40 w-[90px] h-[90px] hidden lg:block' src={star.src} alt="" />
         <img className='absolute w-full h-full top-0 left-0 md:hidden' src={home_bg.src} alt="" />
-        {Number(width) > 980 ? <img id='left_war' className='absolute right-0 top-0 sm:top-[-20px] z-10 lg:top-[10%] lg:w-[250px] lg:h-[450px]' src={right_war_res.src}/> : <img className='absolute right-0 top-0 sm:top-[-20px] z-10 lg:top-[10%] lg:w-[500px] lg:h-[600px]' src={war_right.src}/>}
+        {Number(width) > 980 ? <img id='left_war' className='absolute right-0 top-0 sm:top-[-20px] z-10 lg:top-[35%] lg:w-[250px] lg:h-[450px] ' src={right_war_res.src}/> : <img className='absolute right-0 top-0 sm:top-[-20px] z-10 lg:top-[10%] lg:w-[500px] lg:h-[600px]' src={war_right.src}/>}
         <h1 className='text-white h-fit text-[40px] w-[60%] leading-[50px] sm:w-[90%] sm:text-center md:w-[80%] lg:leading-[100px] lg:text-[60px] lg:w-[690px] '>ARENA AWAITS YOU, <h1 className='text-[#C45260]'>MY LORD</h1></h1>
         <div className="w-full h-[50vh] pt-9 lg:pt-3 flex flex-col items-center gap-14 relative lg:gap-8">
           <p className='leading-[24px] text-[15px] text-[#ffffffae] font-extralight pr-4 sm:w-[75%] sm:text-center sm:pt-7 md:w-[60%] lg:w-[40%] '>
@@ -88,17 +125,46 @@ export default function Home() {
             </div>
           </div>
           <div className='w-full h-fit flex justify-center items-center gap-4 mt-10'>
-            <img className='w-36 h-12 cursor-pointer' src={appstore.src} alt="" />
-            <img className='w-36 h-12 cursor-pointer' src={playstore.src} alt="" />
+            <img className='w-32 h-12 cursor-pointer  sm:w-36' src={appstore.src} alt="" />
+            <img className='w-32 h-12 cursor-pointer sm:w-36' src={playstore.src} alt="" />
           </div>
-          {Number(width) > 980 ? <img id='left_war' className='absolute left-[-16px] lg:w-[300px] lg:h-[450px] top-[-150px]' src={left_war_res.src}/> : <img className='absolute left-[-16px] top-[35%] md:w-[300px] md:top-[20%]' src={war_left.src}/>}
+          {Number(width) > 980 ? <img id='left_war' className='absolute left-[-16px] lg:w-[300px] lg:h-[450px] top-[-20px]' src={left_war_res.src}/> : <img className='absolute left-[-16px] top-[30%] md:w-[300px] md:top-[20%]' src={war_left.src}/>}
         
         </div>
         </div>
       </div>
       
-      <div className='h-[100vh] bg-[#04131B] mt-[-1px]'>
-        <h1>helo</h1>
+      <div className='h-fit bg-[#04131B] mt-[-1px] flex justify-center items-center flex-col lg:pt-10 relative'>
+        <img className='absolute left-0 top-0 hidden lg:block' src={strokes.src} alt="" />
+        <img className='w-40 h-20 lg:w-56' src={abt_btn.src} alt="" />
+        <div className='w-full h-fit flex flex-col text-center gap-12 mt-12 lg:w-[85%] lg:px-12'>
+          <div className="w-full h-1/2 justify-center items-center flex flex-col px-4 lg:flex-row lg:justify-between">
+            <p className='leading-6 flex flex-col gap-8 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-7/12 lg:text-left'>
+            <h2 className='mt-10 font-bold text-white text-[20px]'>Arena master</h2>
+              Arena Master is a tokenized online video game developed by Sambrela. 
+              Sambrela boasts great expertise in blockchain technology and mobile game development. 
+              Our sole aim is to provide a real-life gaming experience for 
+              people in the gaming domain, blockchain technology and the world as a whole.
+              </p>
+              <img className='h-[200px] w-[230px] lg:translate-y-7 lg:h-60 lg:w-80 lg:translate-x-16' src={res_arena_inf.src} alt="" />
+          </div>
+          <div className="w-full h-1/2 justify-center items-center flex flex-col px-4 lg:flex-row-reverse lg:justify-between">
+            <p className='leading-6 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-5/12'>
+                We are a team of amazing people with a sharp focus on building NFTs 
+                and skill-based games in the mobile space and exploring other types of gaming on 
+                different platforms to bring fun and entertainment to the blockchain industry.
+              </p>
+              {Number(width) > 980 ? <img id='characters_img' className='' src={res_arena_char.src} alt="" /> : <img id='characters_img' className='h-[230px] w-[100%] sm:w-[90%] md:w-[60%] lg:w-1/2 lg:h-[270px] lg:translate-y-[-60px]' src={res_arena_char.src} alt="" />}
+              
+          </div>
+        </div>
+      </div>
+      <div className='h-fit bg-[#04131B] pt-9 w-full flex justify-center items-center pb-7'>
+      <div className="w-[92%] h-[526px] bg-gradient-to-br from-[#c4525fd6] to-[#04131B]  rounded-[10px] relative p-[1.5px]">
+        <h1 className='absolute left-1/2 translate-x-[-50%] font-extrabold text-[16px] leading-6 text-white w-48 bg-[#04131B] top-[-10px] text-center'>AMT TOKEN UTILITY</h1>
+        <div className="w-full h-full bg-[#04131B] rounded-[10px]"></div>
+
+        </div>
       </div>
     </main>
   );

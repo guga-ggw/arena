1d511e3dfff3501db4071ceb676e06e774a5f4a2
guga-ggw <ggwomega15@gmail.com>
2024-02-02 20:17:31 +0400

    finnal

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,33 @@ import circle from './assets/crc.png'
 import circle_res from './assets/crc_ress.png'
 import list_res from './assets/list_res.png'
 import team from './assets/team.png'
+import pfp_1 from './assets/pfp1.png'
+import pfp_2 from './assets/pfp2.png'
+import pfp_3 from './assets/pfp3.png'
+import pfp_4 from './assets/pfp4.png'
+import pfp_5 from './assets/pfp5.png'
+import pfp_6 from './assets/pfp6.png'
+import pfp_7 from './assets/pfp7.png'
+import pfp_8 from './assets/pfp8.png'
+import pfp_9 from './assets/pfp9.png'
+import pfp_10 from './assets/pfp10.png'
+import pfp_11 from './assets/pfp11.png'
+import pfp_12 from './assets/pfp12.png'
+import pfp_13 from './assets/pfp13.png'
+import linkdn from './assets/linkdn.png'
+import partners from './assets/partners.png'
+import par1 from './assets/part1.png'
+import par2 from './assets/part2.png'
+import par3 from './assets/part3.png'
+import par4 from './assets/part4.png'
+import par5 from './assets/part5.png'
+import foot_reddit from './assets/foot_reddit.png'
+import foot_telegram from './assets/foot_telegram.png'
+import foot_twitter from './assets/foot_tweet.png'
+import foot_m from './assets/foot_m.png'
+import ref_form from './assets/reg-form.png'
+import toggle from './assets/toggle.png'
+import advisors from './assets/advisors.png'
 
 import './index.scss';
 import useWindowResize from './hooks/useWindowResize';
@@ -69,7 +96,8 @@ import { useState } from 'react';
 
 
 export default function Home() {
-
+  const advisorsData = [pfp_1, pfp_2, pfp_3, pfp_4]
+  const pfpData = [pfp_1, pfp_2, pfp_3, pfp_4, pfp_5, pfp_6, pfp_7, pfp_8, pfp_9, pfp_10, pfp_11, pfp_12, pfp_13]
   const navlist = ['Home', 'About Us', 'Design concept', 'Roadmap', 'Tokenomics', 'Our team', 'Partners', 'Whitepaper', 'Arena Master Deck']
   const token_list = [
     {
@@ -138,7 +166,7 @@ export default function Home() {
             <li className={`font-semibold text-[13px] xl:text-[16px] cursor-pointer ${li == "Home" ? 'text-[#C45260]' : ""} hover:text-[#C45260] transition-colors`}>{li}</li>
             ))}
           </ul>
-          <div className='w-12 h-[24px] bg-black rounded-xl'></div>
+          <img className='cursor-pointer' src={toggle.src} alt="" />
       </nav>
       <div className='min-h-[700px] pb-2 bg-[#04131B] w-full px-4 flex font-black pt-14 flex-col relative md:text-center md:items-center'>
         <img className='absolute right-8 top-40 w-[90px] h-[90px] hidden lg:block' src={star.src} alt="" />
@@ -176,12 +204,12 @@ export default function Home() {
         </div>
       </div>
       
-      <div className='h-fit bg-[#04131B] mt-[-1px] flex justify-center items-center flex-col lg:pt-10 relative'>
+      <div className='h-fit bg-[#04131B] mt-[-1px] flex justify-center items-center flex-col lg:pt-10 relative xl:px-60'>
         <img className='absolute left-0 top-0 hidden lg:block' src={strokes.src} alt="" />
         <img className='w-40 h-20 lg:w-56' src={abt_btn.src} alt="" />
         <div className='w-full h-fit flex flex-col text-center gap-12 mt-12 lg:w-[85%] lg:px-12'>
           <div className="w-full h-1/2 justify-center items-center flex flex-col px-4 lg:flex-row lg:justify-between">
-            <p className='leading-6 flex flex-col gap-8 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-7/12 lg:text-left'>
+            <p className='leading-6 flex flex-col gap-8 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-5/12 lg:text-left'>
             <h2 className='mt-10 font-bold text-white text-[20px]'>Arena master</h2>
               Arena Master is a tokenized online video game developed by Sambrela. 
               Sambrela boasts great expertise in blockchain technology and mobile game development. 
@@ -191,7 +219,7 @@ export default function Home() {
               <img className='h-[200px] w-[230px] lg:translate-y-7 lg:h-60 lg:w-80 lg:translate-x-16' src={res_arena_inf.src} alt="" />
           </div>
           <div className="w-full h-1/2 justify-center items-center flex flex-col px-4 lg:flex-row-reverse lg:justify-between">
-            <p className='leading-6 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-5/12'>
+            <p className='leading-6 text-white text-opacity-60 font-light text-[14px] w-[95%] lg:w-4/12 text-start'>
                 We are a team of amazing people with a sharp focus on building NFTs 
                 and skill-based games in the mobile space and exploring other types of gaming on 
                 different platforms to bring fun and entertainment to the blockchain industry.
@@ -226,12 +254,12 @@ export default function Home() {
         </div>
       </div>
       <div className="mt-[-1px] w-full h-fit pb-28 flex flex-col items-center bg-[#04131B]">
-        <img className='w-56 h-20 lg:mt-52 md:mt-24' src={design_concept.src} alt="" />
+        <img className='w-56 h-20 lg:mt-32 md:mt-24' src={design_concept.src} alt="" />
         <div className='mt-10 flex w-full items-center justify-center relative gap-3 lg:mt-16 flex-col'>
           <img className='absolute left-0 top-[-80px] md:w-12 md:hidden' src={pht_lefr.src} alt="" />
           <div className='flex w-full justify-center xl:gap-6 gap-3'>
             <img className='h-80 cursor-pointer lg:h-96 hidden md:block xl:h-auto  ' src={left_arr.src} alt="" />
-            {Number(width) > 980 ?  <img className='w-[800px] h-96 xl:w-auto xl:h-auto' src={res_pht.src} alt="" />: <img className='h-48 w-80 sm:h-60 sm:w-96  md:w-[550px] md:h-80' src={pht_tem.src} alt="" />}
+            {Number(width) > 980 ?  <img className='w-[800px] h-96 xl:w-auto xl:h-auto' src={res_pht.src} alt="" />: <img className='h-48 w-80 sm:h-60 sm:w-96  md:w-[510px] md:h-80' src={pht_tem.src} alt="" />}
             <img className='h-80 cursor-pointer lg:h-96 hidden md:block xl:h-auto' src={right_arr.src} alt="" />
           </div>
           {Number(width) > 1440 ?  
@@ -384,8 +412,138 @@ export default function Home() {
           
         </div>
       </div>
-      <div className='h-96 flex justify-center'>
-        <img className='w-44 h-20 mt-11 lg:w-64' src={team.src} alt="" />
+      <div className='h-fit flex items-center flex-col pb-16 overflow-hidden'>
+        <img className='w-44 h-20 mt-2 lg:w-64' src={team.src} alt="" />
+        <div className="w-fit gap-6 flex mt-36 justify- lg:w-full lg:flex-wrap lg:px-6 lg:gap-16 lg:justify-center xl:justify-center xl:px-4">
+          {pfpData.map((pfp) => (
+                <div className={`w-52 h-[231px]  bg-gradient-to-b from-[#c4525fd6] to-[#04131B] rounded-md p-[2px] ${pfp == pfp_7 ? "opacity-100" : "opacity-30 md:opacity-100"} lg:mt-6 xl:w-[302px] `}>
+                    <div className="h-full w-full bg-[#04131B] rounded-md relative flex flex-col text-center items-center justify-center">
+                        <img className='absolute top-[-50%] translate-y-[20%] left-1/2 translate-x-[-50%]' src={pfp.src} alt="" />
+                        <h2 className='text-white font-bold leading-6 mt-3'>Giorgi Rurua</h2>
+                        <p className='text-[12px] text-[#BDBDBD]  font-semibold translate-y-2'>Product designer</p>
+                        <img className='absolute bottom-3' src={linkdn.src} alt="" />
+                    </div>
+                </div>
+          ))}
+
+        </div>
+      </div>
+      <div id='partners' className="w-full h-fit flex flex-col items-center pb-32">
+      <img className='w-48 mt-20 lg:w-52' src={partners.src} alt="" />
+      {Number(width) > 980 ?  
+        <div className='flex w-full h-fit py-10 px-20 gap-14 mt-24'>
+          <div className="h-80 w-1/5 flex flex-col items-center justify-around">
+            <img className='' src={par1.src} alt="" />
+            <img className='' src={par1.src} alt="" />
+          </div>
+          <div className="h-80 w-1/5 flex flex-col items-center justify-around">
+            <img className='' src={par2.src} alt="" />
+            <img className='' src={par2.src} alt="" />
+          </div>
+          <div className="h-80 w-1/5 flex flex-col items-center justify-around">
+            <img className='w-' src={par3.src} alt="" />
+            <img className='' src={par3.src} alt="" />
+          </div>
+          <div className="h-80 w-1/5 flex flex-col items-center justify-around">
+            <img className='' src={par4.src}  alt="" />
+            <img className='' src={par4.src} alt="" />
+          </div>
+          <div className="h-80 w-1/5 flex flex-col items-center justify-around">
+            <img className='' src={par5.src}  alt="" />
+            <img className='' src={par5.src} alt="" />
+          </div>
+        </div>
+      : 
+      <>
+        <div className="flex flex-col h-fit w-full mt-20 px-5 gap-10 md:px-24">
+          <div className="flex w-full lg:w-fit justify-start lg:justify-normal">
+            <img className=' md:scale-150' src={par1.src} alt="" />
+          </div>
+          <div className="flex w-full lg:w-fit justify-end lg:justify-normal">
+            <img src={par2.src} alt="" />
+          </div>
+          <div className="flex w-full justify-start lg:w-fit lg:justify-normal">
+            <img className='pl-5 md:scale-150' src={par3.src} alt="" />
+          </div>
+          <div className="flex w-full lg:w-fit  justify-end lg:justify-normal">
+            <img className='md:scale-150' src={par4.src} alt="" />
+          </div>
+          <div className="flex w-full lg:w-fit justify-start lg:justify-normal">
+            <img className='md:scale-150' src={par5.src} alt="" />
+          </div>
+        </div>
+      </>
+      
+      }
+      </div>
+      <div className="w-full h-fit flex flex-col items-center pb-16 overflow-hidden">
+        <img className='w-36 lg:w-52 h-20' src={advisors.src} alt="" />
+        <div className="w-fit gap-6 flex mt-36 justify- lg:w-full lg:flex-wrap lg:px-6 lg:gap-16 lg:justify-center xl:justify-center xl:px-4">
+          {advisorsData.map((pfp) => (
+                <div className={`w-52 h-[231px]  bg-gradient-to-b from-[#c4525fd6] to-[#04131B] rounded-md p-[2px] lg:mt-6 xl:w-[302px] `}>
+                    <div className="h-full w-full bg-[#04131B] rounded-md relative flex flex-col text-center items-center justify-center">
+                        <img className='absolute top-[-50%] translate-y-[20%] left-1/2 translate-x-[-50%]' src={pfp.src} alt="" />
+                        <h2 className='text-white font-bold leading-6 mt-3'>Giorgi Rurua</h2>
+                        <p className='text-[12px] text-[#BDBDBD]  font-semibold translate-y-2'>Product designer</p>
+                        <img className='absolute bottom-3' src={linkdn.src} alt="" />
+                    </div>
+                </div>
+          ))}
+
+        </div>
+      </div>
+      <div className="w-full h-fit flex text-center flex-col items-center px-4 relative mt-52">
+      {Number(width) > 1440 ? 
+      <div className=' h-fit w-fit absolute'>
+        <h1 className='text-white leading-10 text-[30px]  font-bold w-[80%] mt-6 xl:absolute z-50 xl:text-black xl:w-[460px] xl:text-left xl:left-16 xl:text-[44px] xl:top-40'>For partnerships, content creation</h1>
+        <p className='w-full z-50 xl:text-black  xl:absolute xl:w-[460px] xl:top-72 xl:text-[18px] xl:font-semibold xl:text-left xl:left-16 font-semibold text-white text-[14px] leading-6 mt-6 px-3 text-opacity-80 sm:w-[60%]'>
+          Interestingly, there is no limit to growing your skills in-game, 
+          and we ensure this by ranking you based on your skills 
+          and rewarding you with our token as you grow in skills.
+        </p>
+        <img className='' src={ref_form.src} alt="" />
+      </div>
+        : ""}
+        <h1 className='text-white leading-10 xl:hidden text-[30px] font-bold w-[80%] mt-6 xl:absolute z-50 xl:text-black xl:w-[460px] xl:text-left xl:left-[10%] xl:text-[44px] xl:top-40'>For partnerships, content creation</h1>
+        <p className='w-full z-50 xl:hidden xl:text-black xl:absolute xl:w-[460px] xl:top-72 xl:text-[18px] xl:font-semibold xl:text-left xl:left-64 font-semibold text-white text-[14px] leading-6 mt-6 px-3 text-opacity-80 sm:w-[60%]'>
+          Interestingly, there is no limit to growing your skills in-game, 
+          and we ensure this by ranking you based on your skills 
+          and rewarding you with our token as you grow in skills.
+        </p>
+        <div id='reg_form' className="w-[95%] z-40 xl:ml-96 xl:mt-[-50px] bg-white h-[523px] flex flex-col mt-10 rounded-[20px] pt-7 items-center md:w-[550px]">
+          <h1 className='text-[#0A142F] text-[30px] leading-10'>GET IN TOUCH</h1>
+          <input className='w-[90%] h-[60px] border-gray-500 border-[1px] hover:border-[2px]  hover:border-gray-800 outline-none mt-11 pl-6 rounded-lg transition-all'  placeholder='Enter your Name' type="text" />
+          <input className='w-[90%] h-[60px] border-gray-500 border-[1px] hover:border-[2px] hover:border-gray-800 outline-none mt-5 pl-6 rounded-lg transition-all'  placeholder='Enter Your Email' type="text" />
+          <textarea className='w-[90%] h-[165px] border-gray-500 border-[1px] hover:border-[2px] hover:border-gray-800 outline-none mt-5 p-4 rounded-lg transition-all' placeholder='Message'/>
+          <button className='bg-[#C45260] w-[90%] h-12 text-white rounded-xl mt-5'>Get in touch</button>
+        </div>
+      </div>
+      <div className="w-full h-fit flex flex-col mt-28 lg:flex-row py-12 relative items-center lg:items-center lg:justify-between lg:px-2 xl:px-24 xl:gap-6">
+        <h2 className='text-white font-bold md:hidden'>join our community</h2>
+        <div className="flex w-full justify-center gap-5 mt-5 lg:w-fit lg:mt-14">
+            <div className="w-[50px] h-[50px] bg-white rounded-full flex justify-center items-center cursor-pointer">
+              <img className='text-black' src={foot_reddit.src} alt="" />
+            </div>
+            <div className="w-[50px] h-[50px] bg-white rounded-full flex justify-center items-center cursor-pointer">
+              <img className='text-black' src={foot_telegram.src} alt="" />
+            </div>
+            <div className="w-[50px] h-[50px] bg-white rounded-full flex justify-center items-center cursor-pointer">
+              <img className='text-black' src={foot_twitter.src} alt="" />
+            </div>
+            <div className="w-[50px] h-[50px] bg-white rounded-full flex justify-center items-center cursor-pointer">
+              <img className='' src={foot_m.src} alt="" />
+            </div>
+          </div>
+          {Number(width) > 980 ?  <h2 className='text-white text-[14px] font-black mt-14 ml-[5%]'>© 2022 Arena. All rights reserved.</h2> : <></>}
+          <div className="w-full lg:w-fit flex flex-col gap-6 items-center mt-14 lg:mt-0 lg:gap-3 lg:items-start">
+              <h2 className='text-white text-[18px]'>Subscire to our Newsletter</h2>
+              <div className='w-[90%] rounded-lg h-14 bg-white flex md:w-[438px] md:justify-center'>
+                <input className='w-[70%] h-full text-black pl-3 rounded-lg outline-none' placeholder='Enter Your Email' type="text" />
+                <button className='w-[30%] h-full bg-[#C45260] text-white text-[14px] rounded-lg'>Subscribe</button>
+              </div>
+          </div>
+          {Number(width) < 980 ?  <h2 className='text-white text-[14px] font-black mt-14'>© 2022 Arena. All rights reserved.</h2> : <></>}
+
       </div>
     </main>
   );
